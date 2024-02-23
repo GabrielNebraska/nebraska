a4a535bde5c73adbcd576be225f96f2e16459bad
Gabriel_Nebraska <abusentidoramaldavida@gmail.com>
2024-02-23 15:46:02 -0300

    AulaUmVue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {ref} from 'vue'
+
+const contador = ref(0)
+
+function decrementar(){
+  contador.value--
+}
+
+</script>
+
+<template>
+
+
+
+<button @click="contador++">adicionar</button>
+<button @click="contador--">decrementar</button>
+ <p>O valor do contador é {{ contador }}</p>
+</template>
+
+<style scoped>
+</style>
