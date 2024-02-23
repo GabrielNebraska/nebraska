89426680d1ed1fbe66aba6a8578cd2c45705213c
Gabriel_Nebraska <abusentidoramaldavida@gmail.com>
2024-02-23 16:53:44 -0300

    AulaUmVueConceitosBasicos

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,41 @@ import {ref} from 'vue'
 
 const contador = ref(0)
 
-function decrementar(){
+
+function decrementar(){  
+  if (contador.value > 0){  
   contador.value--
 }
 
+}
+
+function reset(){
+  contador.value = 0
+}
 </script>
 
 <template>
-
+   
 
 
 <button @click="contador++">adicionar</button>
-<button @click="contador--">decrementar</button>
- <p>O valor do contador é {{ contador }}</p>
+<button @click="decrementar">decrementar</button>
+<button @click="reset">Resetar</button>
+
+<p>O valor do contador é {{ contador }}</p>
+
 </template>
 
 <style scoped>
+button {
+font-size: 2rem;
+padding: 1rem;
+margin: 1rem;
+cursor: pointer;
+border: none;
+background-color: aliceblue;
+border-radius: 0.5rem;
+
+
+}
 </style>
